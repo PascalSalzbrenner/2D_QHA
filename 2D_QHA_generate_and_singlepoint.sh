9f65e42c495c50540190e016b9e2f7d27fafdde3
Pascal Salzbrenner <pts28@cam.ac.uk>
2023-06-27 10:40:11 +0100

    Refactor to use lattice parameter instead of volume

--- 2D_QHA_generate_and_singlepoint.sh
+++ 2D_QHA_generate_and_singlepoint.sh
@@ -1,14 +1,14 @@
 # script to generate structures where we must optimise both volumes and angles and then do singlepoint calculations on them
 
 # input variables
-# minimum and maximum volumes, volume step
-vmin=$1
-vmax=$2
-vstep=$3
+# minimum and maximum lattice parameters, lattice parameter step
+a_min=$1
+a_max=$2
+a_step=$3
 # minimum and maximum angles, angle step
-amin=$4
-amax=$5
-astep=$6
+alpha_min=$4
+alpha_max=$5
+alpha_step=$6
 # element - currently, single element is supported
 element=$7
 # energy solver - DFT, EDDP
@@ -23,11 +23,18 @@ function generate_R-3m {
 
 mkdir hopper
 
-for vol in `seq $vmin $vmax $vstep`; do
-	for angle in `seq $amin $amax $astep`; do
-		echo "TITL ${element}-${vol}_${angle} 0.000000000000 $vol 0.000000000000  0.00  0.00 1 (R-3m) n - 1" > hopper/${element}-${vol}_${angle}.res
-		
-				
+for a in `seq $a_min $a_max $a_step`; do
+	for alpha in `seq $alpha_min $alpha_max $alpha_step`; do
+		echo "%block lattice_abc" > temp.cell
+		echo "$a $a $a" >> temp.cell
+		echo "$alpha $alpha $alpha" >> temp.cell
+		echo "%endblock lattice_abc" >> temp.cell
+		echo "" >> temp.cell
+		echo "%block positions_frac" >> temp.cell
+		echo "$element 0 0 0" >> temp.cell
+		echo "%endblock positions_frac" >> temp.cell
+
+		cabal cell res < temp.cell > hopper/${element}-a_${a}-alpha_${alpha}.res
 
 	done
 done
