6d31eae05e1eb3ee9bc56bbffeacf24003251f49
Pascal Salzbrenner <pts28@cam.ac.uk>
2023-07-12 15:57:30 +0100

    Add interpolation Python script

---
+++ 2D_QHA_interpolate.py
@@ -0,0 +1,4 @@
+# python script to find the phonon-corrected pressure, and the energies at those pressures for a series of structures with different angles and volumes
+# built to work with my 2D_QHA_generate_and_singlepoint.sh and 2D_QHA_phonons.sh scripts
+
+# written by Pascal Salzbrenner, pts28@cam.ac.uk
